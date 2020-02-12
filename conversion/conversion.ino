d764b2ad46ecac66c5f08b20b643f42e2bbeca52
Jash Jacob <jash1993@gmail.com>
2020-02-12 16:10:35 +0530

    Setup

---
+++ conversion/conversion.ino
@@ -0,0 +1,26 @@
+void setup() {
+
+  //Integer Definition
+    int x1=13;
+    int x10=12;
+    int x=11;
+    int y=10;
+    int z=9;
+    int w=8;
+      
+      //INPUTS
+      pinMode(x1,INPUT);
+      pinMode(x10,INPUT);
+      pinMode(x,INPUT);
+      pinMode(y,INPUT);
+      pinMode(z,INPUT);
+      pinMode(w,INPUT);
+
+      //OUTPUTS
+ }
+
+void loop() {
+
+    //program
+
+}
