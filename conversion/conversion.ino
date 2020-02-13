5be4630c881814c3eca411e6ce9e946727e8e7fe
Jash Jacob <jash1993@gmail.com>
2020-02-13 12:26:22 +0530

    x1 DI - x1DO test loop
    Serial Test Output
    12:25:18.549 -> x1 Digital Output OFF
    12:25:19.040 -> x1 Digital Output OFF
    12:25:19.528 -> x1 Digital Output ON
    12:25:21.541 -> x1 Digital Output OFF
    12:25:22.039 -> x1 Digital Output ON
    12:25:24.027 -> x1 Digital Output ON

--- conversion/conversion.ino
+++ conversion/conversion.ino
@@ -2,56 +2,61 @@
   int value = 0;
   
   //INPUT Integer Definition
-    int x1di=13;
-    int x10di=12;
-    int xdi=11;
-    int ydi=10;
-    int zdi=9;
-    int wdi=8;
+    int x1di=13;  //PIN 13
+    int x10di=12; //PIN 12
+    int xdi=11;   //PIN 11
+    int ydi=10;   //PIN 10
+    int zdi=9;    //PIN 9
+    
 
    //OUTPUT Integer Definition
-   int x1do=7;
-   int x10do=6;
-   int x100do=5;
-   int xdo=4;
-   int ydo=3;
-   int zdo=2;
-   int wdo=1;
+   int x1do=7;    //PIN7
+   int x10do=6;   //PIN6
+   int x100do=5;  //PIN5
+   int xdo=4;     //PIN4
+   int ydo=3;     //PIN3
+   int zdo=2;     //PIN2
+ 
 
 void setup() {
+  
       Serial.begin(9600);
-      
       //DIGITAL INPUTS
-      pinMode(x1di,INPUT);
+      pinMode(x1di,INPUT);  //PIN13
       pinMode(x10di,INPUT);
       pinMode(xdi,INPUT);
       pinMode(ydi,INPUT);
       pinMode(zdi,INPUT);
-      pinMode(wdi,INPUT);
-
+    
       //DIGITAL OUTPUTS
-      pinMode(x1do,OUTPUT);
+      pinMode(x1do,OUTPUT); //PIN7
       pinMode(x10do,OUTPUT);
       pinMode(x100do,OUTPUT);
       pinMode(xdo,OUTPUT);
       pinMode(ydo,OUTPUT);
       pinMode(zdo,OUTPUT);
-      pinMode(wdo,OUTPUT);   
+       
  }
 
 void loop() {
 
-    value = digitalRead(x1di);
-
-    if(value == 0) {
-          digitalWrite(x1do,HIGH);
-          Serial.print(23);
-    }
-
-    if(value == 1) {
-          digitalWrite(x1do,LOW);
-          Serial.print(220);
-    }
-
+      if(digitalRead(x1di)==0){
+        digitalWrite(x1do,LOW);
+        Serial.print("x1 Digital Output OFF");
+        Serial.print("\n");
+        delay(500);
+      }
+       if(digitalRead(x1di)==1){
+        digitalWrite(x1do,HIGH);
+        Serial.print("x1 Digital Output ON");
+        Serial.print("\n");
+        delay(2000);
+      }
     
+    
+
+   
+
+   
+  
 }
